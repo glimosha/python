97072f914f0421289673a7ef40f64a7ba2776da9
glimosha <glimosha@gmail.com>
2018-09-14 17:37:29 +0500

    Успел только easy

---
+++ Homework/Easy03.py
@@ -0,0 +1,39 @@
+
+    __author__ = 'Блинов Андрей Игоревич'
+
+# Задание-1:
+# Напишите функцию, округляющую полученное произвольное десятичное число
+# до кол-ва знаков (кол-во знаков передается вторым аргументом).
+# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# Для решения задачи не используйте встроенные функции и функции из модуля math.
+
+def my_round(number, ndigits):
+    number = number * (10 ** ndigits) + 0.4
+    number = number // 1
+    return number / (10 ** ndigits)
+
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
+
+
+# Задание-2:
+# Дан шестизначный номер билета. Определить, является ли билет счастливым.
+# Решение реализовать в виде функции.
+# Билет считается счастливым, если сумма его первых и последних цифр равны.
+# !!!P.S.: функция не должна НИЧЕГО print'ить
+
+def lucky_ticket(ticket_number):
+    ticket_number = str(ticket_number)
+    ticket_number = list(int(numbers) for numbers in (ticket_number))
+    left = sum(ticket_number[0:3])
+    right = sum(ticket_number[3:6])
+
+    if left == right:
+        return ('Счастливый билет')
+    else:
+        return ("Не повезло")
+
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
